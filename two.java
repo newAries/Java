094dd5da44ef11507fb9a1e5b0c2f8c6d2f282be
Aries <38637323+newAries@users.noreply.github.com>
2019-03-02 17:10:21 +0800

    Add files via upload

---
+++ two.java
@@ -0,0 +1,20 @@
+public class two{
+    public static void main(String a[]){
+        int b=66;
+        if(90<=b&&b<100) {
+            System.out.println("优秀！");
+        }
+        else if(b>=80&&b<90){
+            System.out.println("很好！");
+        }
+        else if(b>=70&&b<80){
+            System.out.println("中等！");
+        }
+        else if (b>=60&&b<70){
+            System.out.println("及格！");
+        }
+        else{
+            System.out.println("不及格！");
+        }
+    }
+}
